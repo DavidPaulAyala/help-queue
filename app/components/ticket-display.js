300f5953c5e31a66317922a71331bf75a6f77c92
Chance Neff and David Ayala <chance@>
2016-10-31 14:17:07 -0700

    add sorting for tickets on teacher page

--- app/components/ticket-display.js
+++ app/components/ticket-display.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  sortBy: ['time:asc'],
+  sortedTickets: Ember.computed.sort('ticket', 'sortBy'),
+
   actions: {
     destroyTicket(ticket) {
       this.sendAction('destroyTicket', ticket);
